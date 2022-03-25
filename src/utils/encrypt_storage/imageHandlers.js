d2caadf7c12c18e79f436b7236e416f065b937cf
Kishor Balgi <kishorbalgi@gmail.com>
2022-03-25 12:15:30 +0530

    Added array buffer to base64 decoder

---
+++ src/utils/encrypt_storage/imageHandlers.js
@@ -0,0 +1,17 @@
+export function _arrayBufferToBase64(buffer) {
+  var binary = "";
+  var bytes = new Uint8Array(buffer);
+  var len = bytes.byteLength;
+  for (var i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return window.btoa(binary);
+}
+export function loadImagePreview(event, id) {
+  console.log(URL.createObjectURL(event.target.files[0]));
+  var output = document.getElementById(id);
+  output.src = URL.createObjectURL(event.target.files[0]);
+  //   output.onload = function () {
+  //     URL.revokeObjectURL(output.src); // free memory
+  //   };
+}
